dd685622d03e0cf0902594274d35b370fc9ec1bc
Claudio <claudio.n.robledo@gmail.com>
2026-05-17 23:41:38 -0300

    Added products catalog test

--- tests/test_saucedemo.py
+++ tests/test_saucedemo.py
@@ -23,3 +23,52 @@ def test_login(driver):
     assert title == "Products"
     # Validar título navegador
     assert "Swag Labs" in driver.title
+
+def test_catalogo_productos(driver):
+    login(
+        driver,
+        "standard_user",
+        "secret_sauce"
+    )
+    title = driver.find_element(
+        By.CLASS_NAME,
+        "title"
+    ).text
+    assert title == "Products"
+
+    # Buscar productos visibles
+    productos = driver.find_elements(
+        By.CSS_SELECTOR,
+        "[data-test='inventory-item']"
+    )
+    assert len(productos) > 0
+
+    # Obtener primer producto
+    nombre = productos[0].find_element(
+        By.CLASS_NAME,
+        "inventory_item_name"
+    ).text
+    precio = productos[0].find_element(
+        By.CLASS_NAME,
+        "inventory_item_price"
+    ).text
+    print(
+        f"\nPrimer producto: {nombre}"
+    )
+    print(
+        f"Precio: {precio}"
+    )
+
+    # Verificar menú
+    menu = driver.find_element(
+        By.ID,
+        "react-burger-menu-btn"
+    )
+    assert menu.is_displayed()
+
+    # Verificar filtro
+    filtro = driver.find_element(
+        By.CLASS_NAME,
+        "product_sort_container"
+    )
+    assert filtro.is_displayed()
